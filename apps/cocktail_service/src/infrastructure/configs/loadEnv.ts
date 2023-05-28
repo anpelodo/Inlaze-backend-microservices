e79313222511d332696a548e01b2265a371687ff
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-05-27 23:57:57 -0500

    chore: change default port for user and cocktail services

--- apps/cocktail_service/src/infrastructure/configs/loadEnv.ts
+++ apps/cocktail_service/src/infrastructure/configs/loadEnv.ts
@@ -1,5 +1,5 @@
 export const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3002,
   dbPort: Number(process.env.DB_PORT) || 5432,
   dbUser: process.env.DB_USER || "postgres",
   dbPassword: process.env.DB_PASSWORD || "password",
--- apps/user_service/src/infrastructure/configs/loadEnv.ts
+++ apps/user_service/src/infrastructure/configs/loadEnv.ts
@@ -1,7 +1,7 @@
 console.log(process.env.DB_PORT, typeof process.env.DB_PORT);
 
 export const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3001,
   dbPort: Number(process.env.DB_PORT) || 5432,
   dbUser: process.env.DB_USER || "postgres",
   dbPassword: process.env.DB_PASSWORD || "password",
