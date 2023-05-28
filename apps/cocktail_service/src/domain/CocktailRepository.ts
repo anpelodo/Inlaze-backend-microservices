2c16a9a315dbce0a16617e04f6705ac1028ceeb0
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-05-28 16:31:29 -0500

    feat(cocktail_service): Add Cocktail Repository interface

---
+++ apps/cocktail_service/src/domain/CocktailRepository.ts
@@ -0,0 +1,23 @@
+import { Cocktail, CocktailCreateDTO, CocktailUpdateDTO } from "./Cocktail";
+
+export interface CocktailRepository {
+  create(cocktail: CocktailCreateDTO): Promise<Cocktail>;
+  getList(
+    sort: CocktailSort,
+    orderBy: CocktailOrderBy,
+    ingredients?: { id: number }[]
+  ): Promise<Cocktail[]>;
+  update(id: number, cocktail: CocktailUpdateDTO): Promise<Cocktail>;
+  logicDelete(id: number): Promise<Cocktail>;
+}
+
+export const enum CocktailSort {
+  ASC = "ASC",
+  DESC = "DESC",
+}
+
+export const enum CocktailOrderBy {
+  CREATED_AT = "CREATED_AT",
+  NAME = "NAME",
+  INGREDIENTS_COUNT = "INGREDIENTS_COUNT ",
+}
