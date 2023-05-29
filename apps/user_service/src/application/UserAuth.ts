0c026ba46ba27bfe27c60fc9d17cb4e4495c5a81
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-05-29 06:02:30 -0500

    refactor(user_service):  Create interface for Token of Crypto

--- apps/user_service/src/application/UserAuth.ts
+++ apps/user_service/src/application/UserAuth.ts
@@ -1,4 +1,4 @@
-import { CryptoService } from "../domain/CryptoService";
+import { CryptoService, Token } from "../domain/CryptoService";
 import { User, UserCreateDTO, UserLoginDTO } from "../domain/User";
 import { UserRepository } from "../domain/UserRepository";
 
@@ -8,10 +8,7 @@ export class UserAuth {
     private readonly crypto: CryptoService
   ) {}
 
-  async login({
-    email,
-    password,
-  }: UserLoginDTO): Promise<{ token: string; user: User } | null> {
+  async login({ email, password }: UserLoginDTO): Promise<AuthData | null> {
     const userToLogin = await this.userRepo.findByEmail(email);
 
     const hashedPswd = this.crypto.hashPassword(password);
@@ -28,18 +25,33 @@ export class UserAuth {
     };
   }
 
-  async signUp({ fullName, email, password, phone }: UserCreateDTO) {
+  async signUp({
+    fullName,
+    email,
+    password,
+    phone,
+  }: UserCreateDTO): Promise<AuthData> {
     if (await this.userRepo.emailExist(email)) {
       //TODO Create a custom Error
       throw Error("Email Exist");
     }
+
     const hashedPswd = this.crypto.hashPassword(password);
 
-    return await this.userRepo.create({
+    const userCreated = await this.userRepo.create({
       fullName,
       email,
       password: hashedPswd,
       phone,
     });
+
+    const token = this.crypto.generateToken(userCreated);
+
+    return {
+      token,
+      user: userCreated,
+    };
   }
 }
+
+export type AuthData = { token: Token; user: User };
--- apps/user_service/src/domain/CryptoService.ts
+++ apps/user_service/src/domain/CryptoService.ts
@@ -3,5 +3,13 @@ import { User } from "./User";
 export interface CryptoService {
   hashPassword(pswd: string): string;
   isPasswordValid(pswd1: string, pswd2: string): boolean;
-  generateToken(user: User): string;
+  generateToken(user: User): Token;
+}
+
+export interface Token {
+  acces_token: string;
+  token_type: string;
+  expires_in?: number;
+  refresh_token?: string;
+  scope?: string;
 }
