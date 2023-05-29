c62732133d4002ab24e491ffe94b4eab4c4be350
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-05-29 02:11:19 -0500

    chore(user_service): Adapt same vocabulary to RoleRepo

--- apps/user_service/src/domain/RoleRepository.ts
+++ apps/user_service/src/domain/RoleRepository.ts
@@ -1,7 +1,7 @@
 import { Role, RoleCreateDTO, RoleUpdateDTO } from "./Role";
 
 export interface RoleRepository {
-  add(role: RoleCreateDTO): Promise<Role>;
+  create(role: RoleCreateDTO): Promise<Role>;
   getList(): Promise<Role[]>;
   findById(id: number): Promise<Role>;
   findByName(name: string): Promise<Role>;
