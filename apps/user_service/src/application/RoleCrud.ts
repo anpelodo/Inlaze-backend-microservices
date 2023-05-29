0cfaf6b719dbe973be70907675583c33668b67be
Juan Felipe Giraldo <juanfegiraldo97@gmail.com>
2023-05-29 02:24:27 -0500

    feat(user_service): Add Role Crud cases

---
+++ apps/user_service/src/application/RoleCrud.ts
@@ -0,0 +1,32 @@
+import { Role, RoleCreateDTO, RoleUpdateDTO } from "../domain/Role";
+import { RoleRepository } from "../domain/RoleRepository";
+
+export class RoleCrud {
+  constructor(private readonly roleRepo: RoleRepository) {}
+
+  async create(role: RoleCreateDTO): Promise<Role> {
+    return this.roleRepo.create(role);
+  }
+
+  async list(): Promise<Role[]> {
+    return this.roleRepo.getList();
+  }
+
+  async findById(id: number): Promise<Role> {
+    return this.roleRepo.findById(id);
+  }
+
+  async update(id: number, partial: RoleUpdateDTO): Promise<Role> {
+    const roleUpdated = await this.roleRepo.update(id, partial);
+
+    return roleUpdated;
+  }
+
+  async delete(id: number): Promise<Role> {
+    const roleToDelete = await this.roleRepo.findById(id);
+
+    await this.roleRepo.delete(roleToDelete);
+
+    return roleToDelete;
+  }
+}
--- apps/user_service/src/domain/RoleRepository.ts
+++ apps/user_service/src/domain/RoleRepository.ts
@@ -4,7 +4,6 @@ export interface RoleRepository {
   create(role: RoleCreateDTO): Promise<Role>;
   getList(): Promise<Role[]>;
   findById(id: number): Promise<Role>;
-  findByName(name: string): Promise<Role>;
-  update(role: RoleUpdateDTO): Promise<Role>;
+  update(id: number, role: RoleUpdateDTO): Promise<Role>;
   delete(role: Role): Promise<Role>;
 }
